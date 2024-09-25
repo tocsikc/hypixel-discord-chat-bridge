60ec67fd831e59cefc189a842f680ca5f0c02830
tocsikc <131706771+tocsikc@users.noreply.github.com>
2024-09-25 17:24:58 +0100

    Update bedwarsStatsCommand.js

--- src/minecraft/commands/bedwarsStatsCommand.js
+++ src/minecraft/commands/bedwarsStatsCommand.js
@@ -31,17 +31,18 @@ class BedwarsCommand extends minecraftCommand {
       if (["overall", "all"].includes(mode)) {
         const { level, finalKills, finalKDRatio, wins, WLRatio, winstreak } = player.stats.bedwars;
         const { broken, BLRatio } = player.stats.bedwars.beds;
+        const { index } = finalKDRatio * finalKDRatio * level;
 
         this.send(
           `/gc [${level}✫] ${player.nickname} FK: ${formatNumber(finalKills)} FKDR: ${finalKDRatio} W: ${formatNumber(
             wins,
-          )} WLR: ${WLRatio} BB: ${formatNumber(broken)} BLR: ${BLRatio} WS: ${winstreak}`,
+          )} WLR: ${WLRatio} BB: ${formatNumber(broken)} BLR: ${BLRatio} WS: ${winstreak} I: ${index}`,
         );
       } else if (mode !== undefined) {
         const { level } = player.stats.bedwars;
         const { finalKills, finalKDRatio, wins, WLRatio, winstreak } = player.stats.bedwars[mode];
         const { broken, BLRatio } = player.stats.bedwars[mode].beds;
-        const { index } = finalKDRatio * finalKDRatio * level
+        const { index } = finalKDRatio * finalKDRatio * level;
 
         this.send(
           `/gc [${level}✫] ${player.nickname} ${capitalize(mode)} F: ${formatNumber(
