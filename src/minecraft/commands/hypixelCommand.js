08f7bb3aa1546cfbca4592d69aabe52b8334c1d5
tocsikc <131706771+tocsikc@users.noreply.github.com>
2024-10-13 20:15:59 +0100

    Update hypixelCommand.js

--- src/minecraft/commands/hypixelCommand.js
+++ src/minecraft/commands/hypixelCommand.js
@@ -22,7 +22,7 @@ class hypixelCommand extends minecraftCommand {
     try {
       username = this.getArgs(message)[0] || username;
       
-      const player = await hypixel.getPlayer(username);
+      const String(player) = await hypixel.getPlayer(username);
       const level = player.level;
       const guild = await hypixel.getGuild("player", player);
       const rank = player.rank;
